311dfc5408f405909713f77e30675e0237354c8f
EinerSanchez <eidavisaqui05@mail.com>
2024-07-22 20:02:37 -0500

    cree una variable donde pide el texto y la funcion de esPalindromo me falta verificar el texto, ignorando los espacion y puntos

--- palindromo.php
+++ palindromo.php
@@ -2,10 +2,16 @@
 //Cree una función llamada “esPalindromo” que reciba una cadena de texto como parámetro y 
 //devuelve true si la cadena es un palíndromo y false si no lo es.  
 
-//ingresar cadena de texto si es en formato palindromo
-$cadena = readline("Ingrese cadena de texto: ");
+// Pedir cadena de texto
+$texto = readline("Ingrese un texto: ");
+function esPalindromo($cadena) {
+  // Eliminar espacios y signos de puntuación
+ 
+  // Convertir a minúsculas
+  
 
-//Creamos funcion de "esPalindromo"
-$palabras = str_word_count($cadena, 1);
+}
 
-print_r($palabras);
+
+
+?>
