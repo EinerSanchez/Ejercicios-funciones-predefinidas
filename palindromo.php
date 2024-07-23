83664f2a747f552a7e70e2404e1d16c1d9015285
EinerSanchez <eidavisaqui05@mail.com>
2024-07-22 20:19:49 -0500

    Cargarndo primer ejercicio finalizado modifique todo lo que no me funcionaba

--- palindromo.php
+++ palindromo.php
@@ -4,15 +4,19 @@
 
 // Pedir cadena de texto
 $texto = readline("Ingrese un texto: ");
+//declaro funcion 
 function esPalindromo($cadena) {
- 
+  // Eliminar espacios y signos de puntuación
+  $cadena = preg_replace('/[^A-Za-z0-9]/', '', $cadena);
+  // Convertir a minúsculas
+  $cadena = strtolower($cadena);
   // Revertir la cadena
   $revertida = strrev($cadena);
   // Comparar la cadena original con la revertida
   return $cadena === $revertida;
 }
 
-// Llamar a la función esPalindromo
+// Llamar a la función 
 if (esPalindromo($texto)) {
   echo "True: '$texto' es un palíndromo.\n";
 } else {
