a39e19366b53ffb33ed7246e932c7ba971ff2b7b
EinerSanchez <eidavisaqui05@mail.com>
2024-07-25 08:49:35 -0500

    Cargando ejercicio cinco modifique la mayoria de codigo para que imprima los numeros mas organizados

--- imprenumeros.php
+++ imprenumeros.php
@@ -3,17 +3,28 @@
 $numeros = range(1, 100);
 //declaro funcion 
 function FierroAlv($numeros) {
+    $peso = [];
+    $pluma = [];
+    $pesopluma = [];
+    $otros = [];
+
     foreach ($numeros as $numero) {
         if ($numero % 3 == 0 && $numero % 5 == 0) {
-            echo "PesoPluma,";
+            $pesopluma[] = $numero;
         } elseif ($numero % 3 == 0) {
-            echo "Peso,";
+            $peso[] = $numero;
         } elseif ($numero % 5 == 0) {
-            echo "Pluma,";
+            $pluma[] = $numero;
         } else {
-            echo "$numero,";
+            $otros[] = $numero;
         }
     }
+
+    echo "PesoPluma=". implode(',', $pesopluma). "\n";
+    echo "Peso=". implode(',', $peso). "\n";
+    echo "Pluma=". implode(',', $pluma). "\n";
+    echo "Otros=". implode(',', $otros). "\n";
 }
 //llamo funcion
 FierroAlv($numeros);
+?>
